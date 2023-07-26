c24550dd636c807aa241ae572c7103958c452c65
Roshan Chaudhary <rosan.91111@gmail.com>
2023-07-26 11:56:32 +0545

    Custom Hooks

--- src/Lists/WorkingLists.jsx
+++ src/Lists/WorkingLists.jsx
@@ -1,35 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./list";
 
 const WorkingLists = () => {
-  const [lists, setlists] = useState([
-    {
-      id: 1,
-      name: "Rosan",
-      age: 21,
-    },
-    {
-      id: 2,
-      name: "Tej",
-      age: 21,
-    },
-    {
-      id: 3,
-      name: "Deepak",
-      age: 21,
-    },
-    {
-      id: 4,
-      name: "Abijeet",
-      age: 24,
-    },
-    {
-      id: 5,
-      name: "Rohan",
-      age: 27,
-    },
-  ]);
-
+  
   const handleDelete = (id) => {
     const newLists = lists.filter((list) => list.id !== id);
     setlists(newLists);
@@ -38,11 +11,14 @@ const WorkingLists = () => {
   return (
     <>
       {/* <BlogList lists={lists} title="Details" /> */}
-      <BlogList
-        lists={lists}
-        title="Details no 2"
-        handleDelete={handleDelete}
-      />
+      {isPending && <div>Loading...</div>}
+      {lists && (
+        <BlogList
+          lists={lists}
+          title="Details no 2"
+          handleDelete={handleDelete}
+        />
+      )}
     </>
   );
 };
